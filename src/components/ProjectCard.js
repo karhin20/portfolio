2387246fcc90ec0cb13c72fe179390c9f2d2554e
karhin20 <kelvinarhin25@gmail.com>
2024-12-05 16:41:30 +0000

    update project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,13 +4,14 @@ export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <a href={link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
           <img src={imgUrl} alt={title} />
           <div className="proj-txtx">
+          <a href={link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
             <h4 style={{ color: 'white' }}>{title}</h4>
             <span style={{ color: 'white' }}>{description}</span>
+            </a>
           </div>
-        </a>
+        
       </div>
     </Col>
   )
