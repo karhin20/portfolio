ede3e9eedab8866a30abdd0461989ba7695c654d
karhin20 <kelvinarhin25@gmail.com>
2024-12-17 04:00:45 +0000

    different lines

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,7 +14,7 @@ export const Banner = () => {
   const [index, setIndex] = useState(1);
 
   const TYPING_SETTINGS = {
-    toRotate: ["Data Analyst", "Web Developer", "AI Application Developer"],
+    toRotate: ["A Data Analyst", "A Web Developer", "An AI Application Developer"],
     period: 2000,
     initialDelay: 500,
     deleteSpeed: 2
@@ -67,7 +67,7 @@ export const Banner = () => {
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>
-                    Hi! I'm Kelvin{' '}
+                    <div>Hi! I'm Kelvin</div>
                     <span 
                       className="txt-rotate" 
                       data-period={TYPING_SETTINGS.period}
